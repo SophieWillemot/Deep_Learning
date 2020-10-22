b37c93f979108bc0338e6984de3e33a0f6b8dc35
Sophie <sophiewillemot72@gmail.com>
2020-10-22 09:44:47 +0200

    Unet ready

--- classification/model/unet.py
+++ classification/model/unet.py
@@ -3,21 +3,20 @@
 import tensorflow as tf
 
 
-class CustomUNet3D(object):
+class CustomUNet2D(object):
     """
     Implements UNet architecture https://arxiv.org/abs/1606.06650
     """
 
     def __init__(self,
                  image_shape,
-                 number_class,
                  filters=(8, 16, 32, 64, 128),
-                 kernel=(3, 3, 3),
+                 kernel=(3, 3),
                  activation=tf.keras.layers.LeakyReLU(),
                  padding='same',
-                 pooling=(2, 2, 2)):
+                 pooling=(2, 2)):
 
-        self.num_classes = number_class
+        #self.num_classes = number_class
         self.image_shape = image_shape
 
         self.filters = filters
@@ -27,27 +26,27 @@ class CustomUNet3D(object):
         self.pooling = pooling
 
     def double_convolution(self, input_, num_filters):
-        layer1 = tf.keras.layers.Conv3D(filters=num_filters,
+        layer1 = tf.keras.layers.Conv2D(filters=num_filters,
                                         kernel_size=self.kernel,
                                         padding=self.padding
                                         )(input_)
         layer2 = self.activation(layer1)
-        layer3 = tf.keras.layers.Conv3D(filters=num_filters,
+        layer3 = tf.keras.layers.Conv2D(filters=num_filters,
                                         kernel_size=self.kernel,
                                         padding=self.padding
                                         )(layer2)
         layer4 = self.activation(layer3)
-        layer5 = tf.keras.layers.SpatialDropout3D(0.2)(layer4)  # adapatation in progress
+        layer5 = tf.keras.layers.SpatialDropout2D(0.2)(layer4)  # adapatation in progress
         return layer5
 
     def maxpooling(self, intput_):
-        layer = tf.keras.layers.MaxPool3D(pool_size=self.pooling,
+        layer = tf.keras.layers.MaxPool2D(pool_size=self.pooling,
                                           padding=self.padding
                                           )(intput_)
         return layer
 
     def upsampling(self, input_):
-        layer = tf.keras.layers.UpSampling3D(size=self.pooling
+        layer = tf.keras.layers.UpSampling2D(size=self.pooling
                                              )(input_)
         return layer
 
@@ -57,8 +56,8 @@ class CustomUNet3D(object):
         return layer
 
     def final_convolution(self, input_):
-        layer = tf.keras.layers.Conv3D(filters=self.num_classes,
-                                       kernel_size=(1, 1, 1),
+        layer = tf.keras.layers.Conv2D(filters=self.num_classes,
+                                       kernel_size=(1, 1),
                                        padding=self.padding
                                        )(input_)
         return layer
@@ -70,7 +69,8 @@ class CustomUNet3D(object):
         layer1 = self.double_convolution(input_, num_filters)
         layer2 = self.maxpooling(layer1)
         layer3 = tf.keras.layers.BatchNormalization()(layer2)
-        return (layer1, layer3)
+        #return (layer1, layer3)
+        return layer3
 
     def bottleneck(self, input_, num_filters):
         layer1 = self.double_convolution(input_, num_filters)
@@ -88,15 +88,17 @@ class CustomUNet3D(object):
         input_ = tf.keras.layers.Input(shape=self.image_shape, dtype=tf.float32, name="input")
 
         x = input_
-        forwards = []
+        #forwards = []
 
         # compression/encoder
         for i in range(len(self.filters) - 1):
-            (forward, x) = self.compression_block(x, num_filters=self.filters[i])
-            forwards.append(forward)
+            #(forward, x) = self.compression_block(x, num_filters=self.filters[i])
+            #forwards.append(forward)
+            x = self.compression_block(x, num_filters=self.filters[i])
 
+        #à voir si ya besoin de l'enlever
         # bottleneck
-        x = self.bottleneck(x, num_filters=self.filters[-1])
+        #x = self.bottleneck(x, num_filters=self.filters[-1])
 
         # pas besoin de l'expansion
         # expansion/decoder
@@ -104,8 +106,25 @@ class CustomUNet3D(object):
         #    x = self.expansion_block(x, forwards[i], num_filters=self.filters[i])
 
         # final layer
-        logits = self.final_convolution(x)
-        output_ = tf.keras.layers.Softmax(name='output')(logits)
-        model = tf.keras.models.Model(input_, output_, name='UNet')
+        #logits = self.final_convolution(x)
+        #x = self.final_convolution(x)
+        #output_ = tf.keras.layers.Softmax(name='output')(logits)
+
+        x = tf.keras.layers.Flatten()(x)
+
+        #final output
+        #left_arm = tf.keras.layers.Softmax(name='left_arm')(x)#(logits)
+        #right_arm = tf.keras.layers.Softmax(name='right_arm')(x)#(logits)
+
+        #head = tf.keras.layers.Softmax(name='head')(x)#(logits)
+        #leg = tf.keras.layers.Softmax(name='leg')(x)#(logits)
+
+        left_arm = tf.keras.layers.Dense(2, activation='softmax', name='left_arm')(x)
+        right_arm = tf.keras.layers.Dense(2, activation='softmax', name = 'right_arm')(x)
+
+        head = tf.keras.layers.Dense(2, activation='softmax', name='head')(x)
+        leg = tf.keras.layers.Dense(2, activation='softmax', name='leg')(x)
+
+        model = tf.keras.models.Model(inputs = input_, outputs = [head, leg, right_arm, left_arm], name='UNet')
 
         return model
